ee59a68d571ed50b2661393dd6fb2f729f5c5080
anupama <anupama_avula@yahoo.com>
2020-05-27 23:38:07 -0400

    final commit

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -8,13 +8,22 @@ import { PersonService } from '../person.service';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  listOfPerson: any;
   
   constructor(private personService : PersonService) { }
 
-  ngOnInit(): void {
-  }
+  // ngOnInit(): void {
+  // }
   get dataList() : Person []{
     return this.personService.getPerson();
  }
+ ngOnInit(): void {
+  this.personService.getPerson().subscribe((persons: any) => {
+    this.listOfPerson = persons;
+  });
+}
+ async delay(ms: number) {
+  await new Promise(resolve => setTimeout(()=>resolve(), ms));
 
+}
 }
--- src/app/person.service.ts
+++ src/app/person.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Person } from './person';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class PersonService {
   private personArr: Person[]= [
     new Person("Jhonny","Maria",true),
     new Person("Amy","Jackson",false),
-    new Person("Lauren","sammy",true)
+    new Person("Lauren","Sammy",true)
 ];
 constructor() { }
 
@@ -17,8 +18,13 @@ constructor() { }
     return this.personArr.filter((Person)=>{return Person.getFirstName().toLowerCase().includes(this.personInput.toLowerCase());
   });
 }
+getPersons(): Observable<Person[]> {
+  return of(PERSON);
+  
+}
   setPerson(formInput: string): void {
     // console.log("From person service:",this.personInput);
     this.personInput = formInput;
   }
+  
  }
